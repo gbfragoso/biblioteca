3cff14060d6d4eb76f1c602d6f5b271bec297781
Gustavo Bonfim Fragoso <gbfragoso94@gmail.com>
2019-09-15 13:42:42 -0300

    Mudanças no AutorModel

--- src/main/java/org/casadeguara/models/AutorModel.java
+++ src/main/java/org/casadeguara/models/AutorModel.java
@@ -2,29 +2,25 @@ package org.casadeguara.models;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.casadeguara.conexao.Conexao;
 import org.casadeguara.entidades.Autor;
-import org.casadeguara.listas.DataSourceProvider;
+
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 
 /**
  * Esta classe gerencia o relacionamento com a tabela Autor no banco de dados.
  * @author Gustavo Fragoso
  */
-public class AutorModel{
+public class AutorModel implements GenericModel<Autor>{
     
     private static final Logger logger = LogManager.getLogger(AutorModel.class);
-    private final DataSourceProvider dataSource;
-    private final IdentificadorModel consultar;
     
-    public AutorModel(DataSourceProvider dataSource) {
-        consultar = new IdentificadorModel();
-        this.dataSource = dataSource;
-    }
-    
+    @Override
     public int atualizar(Autor autor) {
         String nome = autor.getNome();
         String query = "update autor set nome = ? where idautor = ?";
@@ -44,6 +40,7 @@ public class AutorModel{
         return 1;
     }
     
+    @Override
     public int cadastrar(Autor novoAutor) {
         String nome = novoAutor.getNome();
         String query = "insert into autor (nome, data_cadastro) values (?, current_date)";
@@ -60,17 +57,28 @@ public class AutorModel{
         }
         return 1;
     }
-    
-    public void atualizarListaAutores() {
-        dataSource.atualizarListaAutores();
-    }
-    
-    public ObservableList<String> getListaAutores() {
-        return dataSource.getListaAutores();
-    }
-    
-    public int consultarAutor(String nome) {
-        return consultar.idAutor(nome);
-    }
+
+	@Override
+	public ObservableList<Autor> consultar(String nome, int resultados) {
+		String query = "select idautor, nome from autor where unaccent(nome) like unaccent(?) limit ?";
+		ObservableList<Autor> autores = FXCollections.observableArrayList();
+		
+		logger.trace("Iniciando a consulta do autor: " + nome);
+		try (Connection con = Conexao.abrir();
+             PreparedStatement ps = con.prepareStatement(query)) {
+            
+            ps.setString(1, nome);
+            ps.setInt(2, resultados);
+            
+            try(ResultSet rs = ps.executeQuery()) {
+                if (rs.next()) {
+                    autores.add(new Autor(rs.getInt(1), rs.getString(2)));
+                }
+            }
+        } catch (SQLException ex) {
+            logger.fatal("Não foi possível consultar o autor", ex);
+        }
+        return autores;
+	}
     
 }
