5590b3a534d84d2731f78d6f9eda175e136e58aa
Gustavo Bonfim Fragoso <gbfragoso94@gmail.com>
2019-09-15 13:10:38 -0300

    Criação do AutoCompleteTextField e da interface GenericModel<T>

---
+++ src/main/java/org/casadeguara/componentes/AutoCompleteTextField.java
@@ -0,0 +1,79 @@
+package org.casadeguara.componentes;
+
+import java.util.LinkedList;
+import java.util.List;
+
+import org.casadeguara.models.GenericModel;
+
+import javafx.collections.ObservableList;
+import javafx.geometry.Side;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.CustomMenuItem;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class AutoCompleteTextField<T> extends TextField {
+	
+    private ContextMenu suggestions;
+    private GenericModel<T> model;
+	private final int resultados;
+	private T result;
+    
+    public AutoCompleteTextField() {
+        this(null, 5);
+    }
+
+    public AutoCompleteTextField(GenericModel<T> model, int resultados) {
+    	this.resultados = resultados;
+    	
+		suggestions = new ContextMenu();
+    	setContextMenu(suggestions);
+        setEditable(true);
+        
+        if(model != null) {
+        	setModel(model);
+        }
+    }
+
+    /**
+     * Adiciona os resultados da busca ao menu de contexto.
+     * @param text Texto para a busca
+     */
+    private void populateContextMenu(String text) {
+        List<CustomMenuItem> menuItems = new LinkedList<>();
+        ObservableList<T> searchResults = model.consultar(text.toUpperCase(), resultados);
+
+        for (T t : searchResults) {
+        	String result = t.toString();
+            CustomMenuItem menuItem = new CustomMenuItem(new Label(result), true);
+
+            menuItem.setOnAction(event -> {
+            	setText(result);
+				positionCaret(result.length());
+				this.result = t;
+                suggestions.hide();
+            });
+
+            menuItems.add(menuItem);
+        }
+        suggestions.getItems().setAll(menuItems);
+    }
+    
+    public T getResult() {
+		return result;
+	}
+
+	public void setModel(GenericModel<T> model) {
+		this.model = model;
+		
+		textProperty().addListener((value, oldValue, newValue) -> {
+            if (newValue.length() > 2) {
+                if (!suggestions.isShowing()) {
+                    suggestions.show(AutoCompleteTextField.this, Side.BOTTOM, 0, 0);
+                }
+
+                populateContextMenu(newValue);
+            }
+        });
+	}
+}
---
+++ src/main/java/org/casadeguara/models/GenericModel.java
@@ -0,0 +1,9 @@
+package org.casadeguara.models;
+
+import javafx.collections.ObservableList;
+
+public interface GenericModel<T> {
+	int atualizar(T t);
+	int cadastrar(T t);
+	ObservableList<T> consultar(String column, int resultados);
+}
