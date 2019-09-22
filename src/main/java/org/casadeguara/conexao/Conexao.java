0d5566fe93fb380d78b3c43b2667a82a5a294fa6
Gustavo Bonfim Fragoso <gbfragoso94@gmail.com>
2019-09-21 22:00:39 -0300

    Voltando pro Java 8

--- src/main/java/org/casadeguara/conexao/Conexao.java
+++ src/main/java/org/casadeguara/conexao/Conexao.java
@@ -1,10 +1,10 @@
 package org.casadeguara.conexao;
 
+import java.beans.PropertyVetoException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-import com.zaxxer.hikari.HikariConfig;
-import com.zaxxer.hikari.HikariDataSource;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 /**
  * Esta classe se responsabiliza pelo gerenciamento do pool de conexões com o jdbc.
@@ -13,20 +13,27 @@ import com.zaxxer.hikari.HikariDataSource;
 public class Conexao {
 	
 	private static final String CONNECTION_URL = "jdbc:postgresql://localhost/biblioteca";
+	private static final String DRIVER_NAME = "org.postgresql.Driver";
 	private static final String PASSWORD = "j3RdOTZ9";
 	private static final String USERNAME = "postgres";
-	private static HikariConfig config = new HikariConfig();
-	private static HikariDataSource ds;
+	private static ComboPooledDataSource ds = new ComboPooledDataSource();
 	
 	static {
-		config.setJdbcUrl(CONNECTION_URL);
-		config.setUsername(USERNAME);
-		config.setPassword(PASSWORD);
-		config.addDataSourceProperty("cachePrepStmts", "true");
-		config.addDataSourceProperty("prepStmtCacheSize", "250");
-		config.addDataSourceProperty("prepStmtCacheSqlLimit", "2048");
-		config.setMaximumPoolSize(5);
-		ds = new HikariDataSource(config);
+		try {
+			ds.setDriverClass(DRIVER_NAME);
+			ds.setJdbcUrl(CONNECTION_URL);
+			ds.setUser(USERNAME);
+			ds.setPassword(PASSWORD);
+			
+			ds.setInitialPoolSize(5);
+			ds.setMinPoolSize(5);                                     
+			ds.setAcquireIncrement(5);
+			ds.setMaxPoolSize(15);
+			ds.setMaxStatementsPerConnection(3);
+			ds.setIdleConnectionTestPeriod(3000);
+		} catch (PropertyVetoException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private Conexao() {}
